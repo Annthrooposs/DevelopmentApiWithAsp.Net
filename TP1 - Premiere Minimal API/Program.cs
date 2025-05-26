a59bd85fd923b4211972a490f1735abac27f5a26
Annthrooposs <44650433+Annthrooposs@users.noreply.github.com>
2025-05-26 19:23:24 +0200

    Projet "Développement d'API avec ASP.NET" - Module TP 1 - Première Minimal API

--- TP1 - Premiere Minimal API/Program.cs
+++ TP1 - Premiere Minimal API/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Mvc;
+using TP1___Premiere_Minimal_API;
+
+
 
 // ==================================================================================================================================================
 //                                                                                                                                                  !
@@ -13,6 +17,7 @@ WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 // Injection d'un service dans le container d'injection de dépendances ------------------------------------------------------------------------------
 // Enregistrement des Services ----------------------------------------------------------------------------------------------------------------------
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSingleton<TodoService>();
 builder.Services.AddOpenApi();
 
 
@@ -42,6 +47,65 @@ app.UseHttpsRedirection();
 //                                                                 Endpoints GET                                                                    !
 //                                                                                                                                                  !
 // ==================================================================================================================================================
+// Recherche dans la liste des Todos sans critère de recherche --------------------------------------------------------------------------------------
+app.MapGet("/todos",
+(
+
+     [FromServices] TodoService   tds
+) =>
+
+{
+     var todo = tds.GetAll();
+
+     if (todo is not null)
+     {
+
+          return Results.Ok(todo);
+     }
+
+     else
+     {
+
+          return Results.NotFound();
+     }
+});
+
+
+
+
+// Recherche dans la liste des Todos selon un critère de recherche ----------------------------------------------------------------------------------
+app.MapGet("/todos/{p_id:int}",
+(
+
+     //[FromRoute( Name = "id")]    int id,
+     [FromRoute]    int            p_id,
+     [FromServices] TodoService    tds
+) =>
+
+{
+     var todo = tds.GetById(p_id);
+     if (todo is null) return Results.NotFound();
+
+     return Results.Ok(todo);
+});
+
+
+
+app.MapGet("todos/active",
+(
+     [FromServices] TodoService tds
+) =>
+{
+     var todo = tds.GetAll().Where(td => td.EndDate is null);
+     if (todo is not null)
+     {
+          return Results.Ok(todo);
+     }
+     else
+     {
+          return Results.NotFound();
+     }
+});
 
 
 
@@ -52,6 +116,19 @@ app.UseHttpsRedirection();
 //                                                                Endpoints POST                                                                    !
 //                                                                                                                                                  !
 // ==================================================================================================================================================
+app.MapPost("/todos", 
+(
+     [FromBody]     string         p_title,
+     [FromServices] TodoService    tds
+) =>
+{
+
+     var result = tds.Add(p_title);
+     return Results.Ok(result);
+
+     // ou bien (en raccourci)
+     //return Results.Ok(tds.Add(p_title));
+});
 
 
 
@@ -62,6 +139,25 @@ app.UseHttpsRedirection();
 //                                                                 Endpoints PUT                                                                    !
 //                                                                                                                                                  !
 // ==================================================================================================================================================
+app.MapPut("/todos/{p_id:int}",
+(
+     [FromRoute]    int            p_id,
+     [FromBody]     Todo           p_td,
+     [FromServices] TodoService    p_tds
+) =>
+{
+     //var todo = p_tds.GetById(p_id);
+     //if (todo is null) return Results.NotFound();
+
+
+     //// On peut modifier le Todo sans avoir à le supprimer avant.
+     //tds.Update(new Todo(id, td.Title, td.StartDate, td.EndDate));
+     // ou bien en raccourci
+     //tds.Update(td with { Id = id });
+     p_tds.Update(p_id, p_td);
+
+     return Results.NoContent();
+});
 
 
 
@@ -72,6 +168,28 @@ app.UseHttpsRedirection();
 //                                                               Endpoints DELETE                                                                   !
 //                                                                                                                                                  !
 // ==================================================================================================================================================
+app.MapDelete("/todos/{p_id:int}",
+(
+     [FromRoute]    int            p_id,
+     [FromServices] TodoService    tds
+) =>
+{
+     var todo = tds.GetById(p_id);
+     if (todo is null) return Results.NoContent();
+
+
+
+     bool result = tds.Delete(p_id);
+     if (result)
+     {
+          
+          return Results.NoContent();
+     }
+
+
+
+     return Results.NotFound();
+});
 
 
 
---
+++ TP1 - Premiere Minimal API/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TP1___Premiere_Minimal_API;
+
+
+
+public record class Todo(
+     int            Id,
+     string         Title,
+     DateTime       StartDate,
+     DateTime?      EndDate        = null
+);
